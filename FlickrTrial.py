0507c41307e326497dc25d8a7e216a70b8779fdf
orion brody <orion.brody@berkeley.edu>
2018-03-29 12:00:17 -0700

    authenticates using method in flickr core.

--- FlickrTrial.py
+++ FlickrTrial.py
@@ -1,31 +1,26 @@
-import flickrapi 
 
-
-def initialize(api_key, api_secret):
-	
-	return flickrapi.FlickrAPI(api_key, api_secret) #initialize flickr client instance.
-	
-
-def authenticate(flickr):
-
-	# auth = flickrapi.OAuth() #creates an instance of the OAuth class.
-	# frob = auth.getFrob() #frob is used for authentication.
-	# #login_link = auth.loginLink(permission, frob) commented out because unsure what a "permission" is 
-	# return auth.getToken(frob) #token is used for API method calls. 
-
-	flickr.authenticate_via_browser(perms='read')
-
-
-
-
+import flickrapi
+import webbrowser
 
 
 api_key = u'03674b2d1d2a403fcdb92551d4115f2f'
 api_secret = u'89f0e506842b6674'
 
-flickr = initialize(api_key, api_secret)
+flickr = flickrapi.FlickrAPI(api_key, api_secret) #initialize flickr client instance.
+# flickr.get_request_token()
+# flickr.authenticate_via_browser()
+print(flickr.token_valid())
+# url = flickr.auth_url()
+# webbrowser.open(url)
+# token = flickr.get_access_token()
 
-authenticate(flickr)
+# flickr.flickr_oauth.get_request_token()
+# flickr.flickr_oauth.auth_for_test(perms=perms)
+# token = flickr.flickr_oauth.get_access_token()
+# flickr.token_cache.token = token
+
+
+
 
 
 
