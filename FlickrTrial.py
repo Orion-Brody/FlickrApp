e6a39d1e645f40a308ebff8efee2c98e22804106
orion brody <orion.brody@berkeley.edu>
2018-04-07 11:36:49 -0700

    handles input of username.

--- FlickrTrial.py
+++ FlickrTrial.py
@@ -6,18 +6,16 @@ import webbrowser
 api_key = u'03674b2d1d2a403fcdb92551d4115f2f'
 api_secret = u'89f0e506842b6674'
 
-flickr = flickrapi.FlickrAPI(api_key, api_secret) #initialize flickr client instance.
-# flickr.get_request_token()
-# flickr.authenticate_via_browser()
-print(flickr.token_valid())
-# url = flickr.auth_url()
-# webbrowser.open(url)
-# token = flickr.get_access_token()
+user_id = input("Enter Username:") #stores username input as the user_id. 
 
-# flickr.flickr_oauth.get_request_token()
-# flickr.flickr_oauth.auth_for_test(perms=perms)
-# token = flickr.flickr_oauth.get_access_token()
-# flickr.token_cache.token = token
+flickr = flickrapi.FlickrAPI(api_key, api_secret, username=user_id) #initialize flickr client instance.
+
+flickr.authenticate_via_browser() #authenticates the flickr user, has the user check in online. 
+
+print(flickr.trait_names)
+
+
+
 
 
 
